bd9c134b081922553a0dc42fc85e393a9d307d85
Oleksandr Boiko <oleksandr.boiko@i.ua>
2024-03-03 14:47:23 +0200

    reload all

--- service-worker.js
+++ service-worker.js
@@ -2,34 +2,39 @@
 
 const URL = 'index.html';
 const cacheVersion = 1;
-const currentCache = {
-    offline: 'offline-cache-' + cacheVersion
-};
 
 this.addEventListener('install', event => {
     event.waitUntil(
-        caches.open(currentCache.offline).then(function(cache) {
-            return cache.addAll([
-                URL,
-            ]);
-        })
+        caches
+            .open('offline-cache-' + cacheVersion)
+            .then(cache => cache.addAll([URL]))
     );
 });
 
 this.addEventListener('fetch', event => {
-    // request.mode = navigate isn't supported in all browsers
-    // so include a check for Accept: text/html header.
-    if (event.request.mode === 'navigate' || (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))) {
-        event.respondWith(fetch(event.request.url).catch(() => {
-            // Return the offline page
-            return caches.match(URL);
-        }));
-    } else {
-        // Respond with everything else if we can
-        event.respondWith(caches.match(event.request)
-            .then(function (response) {
-                return response || fetch(event.request);
-            })
-        );
-    }
+    // // request.mode = navigate isn't supported in all browsers
+    // // so include a check for Accept: text/html header.
+    // if (event.request.mode === 'navigate' || (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))) {
+    //     event.respondWith(
+    //         fetch(event.request.url)
+    //             .catch(() => {
+    //                 // Return the offline page
+    //                 return caches.match(URL);
+    //             })
+    //     );
+    // } else {
+    //     // Respond with everything else if we can
+    //     event.respondWith(
+    //         caches
+    //             .match(event.request.url)
+    //             .then(function (response) {
+    //                 return response || fetch(event.request.url);
+    //             })
+    //     );
+    // }
+
+    event.respondWith(
+        fetch(event.request.url)
+            .catch(() => caches.match(URL))
+    );
 });
